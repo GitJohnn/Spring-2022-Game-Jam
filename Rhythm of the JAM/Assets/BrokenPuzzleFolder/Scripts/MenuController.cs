e714a38907ec8f5ae9aa6c8a6056f743497924ad
GitJohnn <39565975+GitJohnn@users.noreply.github.com>
2022-03-12 21:03:36 -0600

    Added Initial Game Loop

---
+++ Rhythm of the JAM/Assets/BrokenPuzzleFolder/Scripts/MenuController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject mainMenu, gameScoresPanel, PausePanel, OptionsPanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        gameScoresPanel.SetActive(false);
+        PausePanel.SetActive(false);
+    }
+
+    public void SinglePlayer()
+    {
+        mainMenu.SetActive(false);
+        gameScoresPanel.SetActive(true);
+        PausePanel.SetActive(false);
+        GameManager.instance.StartGame();
+    }
+
+    public void Multiplayer()
+    {
+
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void PauseGame()
+    {
+        PausePanel.SetActive(true);
+        gameScoresPanel.SetActive(true);
+        mainMenu.SetActive(false);
+        GameManager.instance.UpdatePauseMusic();
+        Time.timeScale = 0;
+    }
+
+    public void UnPauseGame()
+    {
+        PausePanel.SetActive(false);
+        gameScoresPanel.SetActive(true);
+        mainMenu.SetActive(false);
+        GameManager.instance.UpdatePauseMusic();
+        Time.timeScale = 1;
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+}
