4d88e80e773b34eba5de7c9dad64ebd2373cf7f1
Tarun Punhani <tpunhani@gmail.com>
2022-03-15 09:04:40 -0500

    Fixed menu

--- Rhythm of the JAM/Assets/BrokenPuzzleFolder/Scripts/MainMenu.cs
+++ Rhythm of the JAM/Assets/BrokenPuzzleFolder/Scripts/MainMenu.cs
@@ -2,11 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
-{
+{   
+
+    public GameObject gameStartButton, multiplayerStartButton, optionsButton, quitButton;
+
+    void Awake(){
+        //clear selected gameobject
+        EventSystem.current.SetSelectedGameObject(null);
+        //set selected gameobject
+        EventSystem.current.SetSelectedGameObject(gameStartButton);
+    }
     public void StartGameButton(){
+        //clear selected gameobject
+        EventSystem.current.SetSelectedGameObject(null);
+        //set selected gameobject
+        EventSystem.current.SetSelectedGameObject(multiplayerStartButton);
+
         SceneManager.LoadScene("GameScene");
-        GameManager.instance.StartGame();
+
+        // GameManager.instance.StartGame();
+    }
+
+    public void MultiPlayerStartGame(){
+        //clear selected gameobject
+        EventSystem.current.SetSelectedGameObject(null);
+        //set selected gameobject
+        EventSystem.current.SetSelectedGameObject(optionsButton);
+
+    }
+
+    public void OptionsButton(){
+        //clear selected gameobject
+        EventSystem.current.SetSelectedGameObject(null);
+        //set selected gameobject
+        EventSystem.current.SetSelectedGameObject(quitButton);
+
+
+    }
+
+
+    public void QuitButton(){
+        //clear selected gameobject
+        EventSystem.current.SetSelectedGameObject(null);
+        //set selected gameobject
+        EventSystem.current.SetSelectedGameObject(gameStartButton);
+
     }
 }
